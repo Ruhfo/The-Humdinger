2c380d46d96a221b6e1ebba0e8b4d87e03b6b89c
nowittyusername <nowittyusername@users.noreply.github.com>
2016-07-24 20:03:47 +0300

    Added menu and QR reading

--- Android src/Humdinger/app/src/main/java/com/humdinger/humdinger/GameActivity.java
+++ Android src/Humdinger/app/src/main/java/com/humdinger/humdinger/GameActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     ControllerView drawingView;
 
---
+++ Android src/Humdinger/app/src/main/java/com/humdinger/humdinger/MenuActivity.java
@@ -0,0 +1,79 @@
+package com.humdinger.humdinger;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+    private final String LOG_TAG = MenuActivity.class.getSimpleName();
+    String ipAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+    public void toSettingsActivity(View view){
+        startActivity(new Intent(this,SettingsActivity.class));
+    }
+
+    public void toProfilesActivity(View view){
+        startActivity(new Intent(this,ProfilesActivity.class));
+    }
+
+    public void toGameActivity(View view){
+        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+        intent.setPackage("com.google.zxing.client.android");
+        intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+        // Verify that the intent will resolve to an activity
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent,0);
+        }
+        //If not then ask the user to input the ip manually
+        else{
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Input server ip");
+            // Set up the input
+            final EditText input = new EditText(this);
+            // Specify the type of input expected;
+            input.setInputType(InputType.TYPE_CLASS_TEXT| InputType.TYPE_CLASS_NUMBER);
+            builder.setView(input);
+            // Set up the buttons
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ipAddress = input.getText().toString();
+                    //Check if the user actually entered something
+                    if(!ipAddress.isEmpty()){
+                        startActivity(new Intent(getApplicationContext(), GameActivity.class));
+                    }else dialog.cancel();
+
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //If everything goes OK with reading the QR data
+        if(resultCode == RESULT_OK){
+            ipAddress = data.getStringExtra("SCAN_RESULT"); // This will contain scan result
+            startActivity(new Intent(this, GameActivity.class));
+        }
+    }
+}
