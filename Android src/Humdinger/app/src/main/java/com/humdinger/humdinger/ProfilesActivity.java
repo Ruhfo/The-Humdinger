676a9649960f8479580e0097daaac5862b3f28bd
nowittyusername <nowittyusername@users.noreply.github.com>
2016-08-25 17:50:28 +0300

    More work on the profiles

--- Android src/Humdinger/app/src/main/java/com/humdinger/humdinger/ProfilesActivity.java
+++ Android src/Humdinger/app/src/main/java/com/humdinger/humdinger/ProfilesActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -22,14 +26,27 @@ public class ProfilesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profiles);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        ListView listView = (ListView) findViewById(R.id.list);
 
+        //Select profile
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(ProfilesActivity.this, "This selects the new profile nr "+i,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Add profile
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(ProfilesActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ProfilesActivity.this, "This adds a new profile", Toast.LENGTH_SHORT).show();
             }
         });
+
+        //Edit or delete profile
+        registerForContextMenu(listView);
 
         profiles.add("SNES");
         profiles.add("Test1");
@@ -43,16 +60,47 @@ public class ProfilesActivity extends AppCompatActivity {
         profiles.add("Test9");
 
         profilesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, profiles);
-        ListView rootView = (ListView) findViewById(R.id.list);
-        rootView.setAdapter(profilesAdapter);
+        listView.setAdapter(profilesAdapter);
 
-        Scanner scanner = new Scanner(getResources().openRawResource(R.raw.snes));
+//        Scanner scanner = new Scanner(getResources().openRawResource(R.raw.snes));
+//
+//        while (scanner.hasNext()) {
+//            String word = scanner.next();
+//            Log.v(LOG_TAG, word);
+//        }
+//        scanner.close();
 
-        while (scanner.hasNext()) {
-            String word = scanner.next();
-            Log.v(LOG_TAG, word);
+    }
+
+    @Override
+    //Called when a Long-Click happens on a listView item
+    //then creates a context Menu using a custom menu layout
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_profiles, menu);
+    }
+
+    //Defines the behaviour of the click depending on what
+    //menu item the user clicked on
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.title_edit:
+                Toast.makeText(ProfilesActivity.this,"You click on edit ",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.title_delete:
+                Toast.makeText(ProfilesActivity.this,"Removed: "+profiles.get(info.position),Toast.LENGTH_SHORT).show();
+                profiles.remove(info.position);
+                profilesAdapter.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
         }
-        scanner.close();
+    }
 
+    public void send(String option){
+        Log.e(LOG_TAG,"Vajutasid "+option);
     }
 }
